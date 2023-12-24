9c6ddf4e9c43d7a2158f99ca53789e096f81cf25
toddwint <todd.wintermute@gmail.com>
2023-12-24 17:49:43 -0600

    updated ttyd to 1.7.4; added aarch64 support; added menu

--- build/app/scripts/startup.sh
+++ build/app/scripts/startup.sh
@@ -9,18 +9,23 @@ echo $HOSTNAME > /etc/hostname
 # Extract compressed binaries and move binaries to bin
 if [ -e /opt/"$APPNAME"/scripts/.firstrun ]; then
     # Unzip frontail and tailon
-    gunzip /usr/local/bin/frontail.gz
+    if [[ $(arch) == "x86_64" ]]; then
+        gunzip /usr/local/bin/frontail.gz
+    fi
     gunzip /usr/local/bin/tailon.gz
 
     # Copy python scripts to /usr/local/bin and make executable
     cp /opt/"$APPNAME"/scripts/ipcalc.py /usr/local/bin
+    cp /opt/"$APPNAME"/scripts/menu /usr/local/bin
     chmod 755 /usr/local/bin/ipcalc.py
+    chmod 775 /usr/local/bin/menu
 fi
 
 # Link scripts to debug folder as needed
 if [ -e /opt/"$APPNAME"/scripts/.firstrun ]; then
     ln -s /opt/"$APPNAME"/scripts/tail.sh /opt/"$APPNAME"/debug
     ln -s /opt/"$APPNAME"/scripts/tmux.sh /opt/"$APPNAME"/debug
+    ln -s /opt/"$APPNAME"/scripts/menu /opt/"$APPNAME"/debug
 fi
 
 # Create the file /var/run/utmp or when using tmux this error will be received
@@ -153,6 +158,10 @@ if [ -e /opt/"$APPNAME"/scripts/.firstrun ]; then
     cp /opt/"$APPNAME"/configs/vsftpd.conf /etc/vsftpd.conf
     cp /opt/"$APPNAME"/configs/tftpd-hpa /etc/default/tftpd-hpa
     cp /opt/"$APPNAME"/configs/webfsd.conf /etc/webfsd.conf
+    cp /opt/"$APPNAME"/configs/tmux.conf /root/.tmux.conf
+
+    # Create menu.json
+    /opt/"$APPNAME"/scripts/make_menujson.py /opt/"$APPNAME"/scripts/menu.json
 fi
 
 # Remove service pid(s) if it exists.
@@ -183,31 +192,55 @@ fi
 
 # Start web interface
 NLINES=1000 # how many tail lines to follow
-
-# ttyd1 (tail and read only)
-# to remove color add the option `-T xterm-mono`
-# selection changed to selectionBackground in 1.7.2 - bug reported
-# `-t 'theme={"foreground":"black","background":"white", "selection":"#ff6969"}'` # 69, nice!
-# `-t 'theme={"foreground":"black","background":"white", "selectionBackground":"#ff6969"}'`
 sed -Ei 's/tail -n 500/tail -n '"$NLINES"'/' /opt/"$APPNAME"/scripts/tail.sh
-nohup ttyd -p "$HTTPPORT1" -R -t titleFixed="${APPNAME}.log" -t fontSize=16 -t 'theme={"foreground":"black","background":"white", "selectionBackground":"#ff6969"}' -s 2 /opt/"$APPNAME"/scripts/tail.sh >> /opt/"$APPNAME"/logs/ttyd1.log 2>&1 &
-
-# ttyd2 (tmux with color)
-# to remove color add the option `-T xterm-mono`
-# selection changed to selectionBackground in 1.7.2 - bug reported
-# `-t 'theme={"foreground":"black","background":"white", "selection":"#ff6969"}'` # 69, nice!
-# `-t 'theme={"foreground":"black","background":"white", "selectionBackground":"#ff6969"}'`
-cp /opt/"$APPNAME"/configs/tmux.conf /root/.tmux.conf
 sed -Ei 's/tail -n 500/tail -n '"$NLINES"'/' /opt/"$APPNAME"/scripts/tmux.sh
-nohup ttyd -p "$HTTPPORT2" -t titleFixed="${APPNAME}.log" -t fontSize=16 -t 'theme={"foreground":"black","background":"white", "selectionBackground":"#ff6969"}' -s 9 /opt/"$APPNAME"/scripts/tmux.sh >> /opt/"$APPNAME"/logs/ttyd2.log 2>&1 &
-
-# frontail
-nohup frontail -n "$NLINES" -p "$HTTPPORT3" /opt/"$APPNAME"/logs/"$APPNAME".log >> /opt/"$APPNAME"/logs/frontail.log 2>&1 &
-
-# tailon
 sed -Ei 's/\$lines/'"$NLINES"'/' /opt/"$APPNAME"/configs/tailon.toml
 sed -Ei '/^listen-addr = /c listen-addr = [":'"$HTTPPORT4"'"]' /opt/"$APPNAME"/configs/tailon.toml
-nohup tailon -c /opt/"$APPNAME"/configs/tailon.toml /opt/"$APPNAME"/logs/"$APPNAME".log /opt/"$APPNAME"/logs/vsftpd_xfers.log /etc/dhcp/dhcpd.conf /etc/vsftpd.conf /etc/default/tftpd-hpa /var/lib/dhcp/dhcpd.leases /opt/"$APPNAME"/logs/ttyd1.log /opt/"$APPNAME"/logs/ttyd2.log /opt/"$APPNAME"/logs/frontail.log /opt/"$APPNAME"/logs/tailon.log >> /opt/"$APPNAME"/logs/tailon.log 2>&1 &
+
+# ttyd1 (tail and read only)
+nohup ttyd \
+    --port "$HTTPPORT1" \
+    --client-option titleFixed="${APPNAME}.log" \
+    --client-option fontSize=16 \
+    --client-option 'theme={"foreground":"black","background":"white","selectionBackground":"#ff6969"}' \
+    --signal 2 \
+    /opt/"$APPNAME"/scripts/tail.sh \
+    >> /opt/"$APPNAME"/logs/ttyd1.log 2>&1 &
+
+# ttyd2 (tmux and interactive)
+nohup ttyd \
+    --writable \
+    --port "$HTTPPORT2" \
+    --client-option titleFixed="${APPNAME}.log" \
+    --client-option fontSize=16 \
+    --client-option 'theme={"foreground":"black","background":"white","selectionBackground":"#ff6969"}' \
+    --signal 9 \
+    /opt/"$APPNAME"/scripts/tmux.sh \
+    >> /opt/"$APPNAME"/logs/ttyd2.log 2>&1 &
+
+# frontail
+if [[ $(arch) == "x86_64" ]]; then
+    nohup frontail \
+        -n "$NLINES" \
+        -p "$HTTPPORT3" \
+        /opt/"$APPNAME"/logs/"$APPNAME".log \
+        >> /opt/"$APPNAME"/logs/frontail.log 2>&1 &
+fi
+
+# tailon
+nohup tailon \
+    -c /opt/"$APPNAME"/configs/tailon.toml \
+    /opt/"$APPNAME"/logs/"$APPNAME".log \
+    /opt/"$APPNAME"/logs/vsftpd_xfers.log \
+    /etc/dhcp/dhcpd.conf \
+    /etc/vsftpd.conf \
+    /etc/default/tftpd-hpa \
+    /var/lib/dhcp/dhcpd.leases \
+    /opt/"$APPNAME"/logs/ttyd1.log \
+    /opt/"$APPNAME"/logs/ttyd2.log \
+    /opt/"$APPNAME"/logs/frontail.log \
+    /opt/"$APPNAME"/logs/tailon.log \
+    >> /opt/"$APPNAME"/logs/tailon.log 2>&1 &
 
 # Remove the .firstrun file if this is the first run
 if [ -e /opt/"$APPNAME"/scripts/.firstrun ]; then
